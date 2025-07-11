39f53b06e201aac0aeda9aab9c058a78156721b9
Hamad Alaslani <hamadsmma@gmail.com>
2025-07-11 10:26:28 +0300

    Add wave functionality for enemies and powerups

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,19 +6,38 @@ public class SpawnManager : MonoBehaviour
     // Start is called once bef
     // publicore the first execution of Update after the MonoBehaviour is created
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
     void Start()
     {
-
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), transform.rotation);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
 
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
     }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), transform.rotation);
+
+        }
+    }
+
 
     private Vector3 GenerateSpawnPosition()
     {
